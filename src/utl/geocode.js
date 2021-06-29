deb6eee0e2835e78d5e0fcb46d38407a17473089
Nathan <okolonathanchinedu@gmail.com>
2021-06-29 17:44:16 +0100

    corrected lon and lat issues

--- src/utl/geocode.js
+++ src/utl/geocode.js
@@ -4,15 +4,15 @@ const geocode = (address,callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoibmF0aGFuLWpzIiwiYSI6ImNrcDZzdmt3bDB0cHoyd3F0djJuOHAxNWkifQ.Fd5KTnIJzrIDBTAWukjlXw&limit=1"
     request({url,json:true},(error,{body}={}) => {
         if (error){
-            callback("Unable connect to weather service. Pls check your Network connection or your code")
+            return callback("Unable connect to weather service. Pls check your Network connection or your code")
         }
         else if (body.features.length === 0){
-            callback("Unable to decode address Pls check the address!")
+            return callback("Unable to decode address Pls check the address!")
         }
         else{
             callback(undefined,{
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[0],
+                longitude: body.features[0].center[1],
                 location: body.features[0].place_name
             })
         }
